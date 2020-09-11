45264ddc30eed0eaa083465bb655c65a861b4847
Tuan Minh <sad361975@gmail.com>
2020-09-11 10:39:05 +0700

    Update feed and discover

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -7,18 +7,18 @@ class PhotosController < ApplicationController
 
 	def create
 		# byebug
-		@photo = Photo.create(user_params)
-		if @photo.save
+		@photo = Photo.create(photo_params)
+		if @photo.valid?
 			flash[:success] = "Save Successfully!"
-			redirect_to new_user_photo_path
+			redirect_to user_path(id: @user.id)
 		else
-			flash[:error] = user_params
+			flash[:error] = @photo.errors.messages
 			redirect_to new_user_photo_path
 		end
 	end
 
 	private
-		def user_params
+		def photo_params
 			params.require(:photo).permit(:title, :sharing_mode, :image, :description, :user_id)
 		end
 
